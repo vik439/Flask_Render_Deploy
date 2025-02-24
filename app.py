a3afdb5f3153651dceaab20bcb021e32d7267992
vik439 <vik439@gmail.com>
2025-02-24 13:25:52 +0530

    Made changes in file

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Render! Your Flask App is Live!"
+    return "Hello, and welcome to your first Flask Render app, Vikash"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Render provides the PORT variable
